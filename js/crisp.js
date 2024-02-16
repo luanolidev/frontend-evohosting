1f44fa08e5b378c06467d38336058f0929df32c2
luanolidev <luanoliveirapardin@gmail.com>
2024-02-16 04:58:23 -0300

    Update frontend

--- js/crisp.js
+++
@@ -1,7 +0,0 @@
-window.$crisp=[];
-window.CRISP_WEBSITE_ID="8accd311-3625-472a-97cd-54e41fbf0424";
-(function(){d=document;s=d.createElement("script");
-s.src="https://client.crisp.chat/l.js";
-s.async=1;
-d.getElementsByTagName("head")[0].appendChild(s);
-})();
